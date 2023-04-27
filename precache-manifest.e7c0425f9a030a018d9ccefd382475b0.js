13e97c56f1aa584a7e7ba21461db008d75b843a1
mohsenghorbanipour <mohsenghorban45@gmail.com@gmail.com>
2023-04-28 02:20:51 +0330

    Updates

---
+++ precache-manifest.e7c0425f9a030a018d9ccefd382475b0.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "a875ef7d26c3b7b5ae69",
+    "url": "/Carousel-Slider/static/css/main.6dc10445.chunk.css"
+  },
+  {
+    "revision": "a875ef7d26c3b7b5ae69",
+    "url": "/Carousel-Slider/static/js/main.a875ef7d.chunk.js"
+  },
+  {
+    "revision": "0aa81f9aa8dfc0a8047a",
+    "url": "/Carousel-Slider/static/css/1.40a3a590.chunk.css"
+  },
+  {
+    "revision": "0aa81f9aa8dfc0a8047a",
+    "url": "/Carousel-Slider/static/js/1.0aa81f9a.chunk.js"
+  },
+  {
+    "revision": "9c24caf7f7a9f7fa5820",
+    "url": "/Carousel-Slider/static/js/runtime~main.9c24caf7.js"
+  },
+  {
+    "revision": "59f4d1efb882349bf91b1048dabd7248",
+    "url": "/Carousel-Slider/static/media/vid1.59f4d1ef.mp4"
+  },
+  {
+    "revision": "d89d8a3343404f2c94856fb9a0b30aa9",
+    "url": "/Carousel-Slider/static/media/vid2.d89d8a33.mp4"
+  },
+  {
+    "revision": "69fc6b3b079b20b1c19388c06d62499a",
+    "url": "/Carousel-Slider/static/media/vid3.69fc6b3b.mp4"
+  },
+  {
+    "revision": "a578f6644c9868f526bdc0f95bd40308",
+    "url": "/Carousel-Slider/static/media/vid4.a578f664.mp4"
+  },
+  {
+    "revision": "4d0f06430ff11e75bee419aa2469603c",
+    "url": "/Carousel-Slider/static/media/vid5.4d0f0643.mp4"
+  },
+  {
+    "revision": "5f54e6396208c7b7911bc0be04af46af",
+    "url": "/Carousel-Slider/static/media/vid6.5f54e639.mp4"
+  },
+  {
+    "revision": "d390f066b4b89980b511391908b635ba",
+    "url": "/Carousel-Slider/static/media/vid7.d390f066.mp4"
+  },
+  {
+    "revision": "4621b744b3101645241e70ecb055e8b9",
+    "url": "/Carousel-Slider/static/media/vid8.4621b744.mp4"
+  },
+  {
+    "revision": "cfa752173f80ff8b94ec223d3b17f7ba",
+    "url": "/Carousel-Slider/static/media/vid9.cfa75217.mp4"
+  },
+  {
+    "revision": "c9359559107a9a91d04040c95c81d7b9",
+    "url": "/Carousel-Slider/static/media/vid10.c9359559.mp4"
+  },
+  {
+    "revision": "4841857eff4dffc5151a7e6f61200ef4",
+    "url": "/Carousel-Slider/static/media/vid11.4841857e.mp4"
+  },
+  {
+    "revision": "aa172ad2900e9d2773cb1c9d8152b248",
+    "url": "/Carousel-Slider/static/media/vid12.aa172ad2.mp4"
+  },
+  {
+    "revision": "e8ca8722d59d11d9520d1cb1e22d69f9",
+    "url": "/Carousel-Slider/static/media/vid13.e8ca8722.mp4"
+  },
+  {
+    "revision": "a7e1e6fd73506392255479f2ae373f60",
+    "url": "/Carousel-Slider/static/media/vid14.a7e1e6fd.mp4"
+  },
+  {
+    "revision": "035f00734798eb92d160a720fccc9002",
+    "url": "/Carousel-Slider/static/media/vid15.035f0073.mp4"
+  },
+  {
+    "revision": "3a39cddf9ddd01d3be258a6f57ba9cd9",
+    "url": "/Carousel-Slider/static/media/vid16.3a39cddf.mp4"
+  },
+  {
+    "revision": "7e63149c260ae3d750fbd431e6e8fdde",
+    "url": "/Carousel-Slider/static/media/vid17.7e63149c.mp4"
+  },
+  {
+    "revision": "cb357268a12d64f199697b304886176d",
+    "url": "/Carousel-Slider/static/media/vid18.cb357268.mp4"
+  },
+  {
+    "revision": "8c85a5122c844c13ca6c69cd54eba6a0",
+    "url": "/Carousel-Slider/static/media/vid19.8c85a512.mp4"
+  },
+  {
+    "revision": "31576de602424e76dedf2c733d47dcf6",
+    "url": "/Carousel-Slider/static/media/vid20.31576de6.mp4"
+  },
+  {
+    "revision": "208320684003e964760544471663e494",
+    "url": "/Carousel-Slider/static/media/vid21.20832068.mp4"
+  },
+  {
+    "revision": "853eb017dc155a070eef6d9b1780fd11",
+    "url": "/Carousel-Slider/static/media/vid22.853eb017.mp4"
+  },
+  {
+    "revision": "e0b935f0cd6aea9806403ab4162fe37d",
+    "url": "/Carousel-Slider/static/media/vid23.e0b935f0.mp4"
+  },
+  {
+    "revision": "b2d836af6b1912e54c56c066b0ddc735",
+    "url": "/Carousel-Slider/static/media/vid24.b2d836af.mp4"
+  },
+  {
+    "revision": "a216b5e48278c0bd70155da8fd08cad2",
+    "url": "/Carousel-Slider/static/media/vid25.a216b5e4.mp4"
+  },
+  {
+    "revision": "7a92361b8b30bb70c8f5a748c9878ac3",
+    "url": "/Carousel-Slider/static/media/vid26.7a92361b.mp4"
+  },
+  {
+    "revision": "d04d4ece45e2b98474ca52e7552c09f8",
+    "url": "/Carousel-Slider/index.html"
+  }
+];
